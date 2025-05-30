4dd0ef69b2aa3f2c56f93e78f027473deba303e0
av1112-verma <ashishverma@pearlorganisation.com>
2025-05-30 10:29:27 +0530

    chnage s

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,6 +27,26 @@ const Home = () => {
         </div>
       </div>
     </section>
+    <section className='our_client py-3 py-5'>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="our_client_img">
+              <img src="https://s3.mordorintelligence.com/home/industry_reports_logo.webp" alt="" />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="our_client_content">
+              <h5>INDUSTRY REPORTS</h5>
+              <h3>Gain a deeper understanding of global markets.</h3>
+              <p>Get access to defining indicators and understand how trends shape the future of markets. Our set of 17,000+ reports cover more than 20 industry verticals.</p>
+              <p>In each report we break down and analyze the market by segments, understand the prevailing dynamics and study the companies operating in these spaces, to finally arrive at solutions that help you have the competitive advantage.</p>
+              <button>View Reports</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     </>
   );
 };
